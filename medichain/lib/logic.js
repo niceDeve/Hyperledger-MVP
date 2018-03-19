3759bf9db4be82fd3fc81450f19ba1babd1fc0a9
nicedeve <bdawn0878@gmail.com>
2018-03-19 13:24:34 +0000

    Adding initial business network definition, grant access logic and updated model file

--- medichain/lib/logic.js
+++ medichain/lib/logic.js
@@ -1,23 +1,44 @@
 'use strict';
 /**
- * Write your transction processor functions here
- */
-
-/**
- * Sample transaction
- * @param {org.medichain.mvp.ChangeAssetValue} changeAssetValue
+ * A medical practicioner grants access to Data Accessors / Other participants
+ * @param {org.medichain.mvp.GrantAccess} grantAccess - the function to grant access
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org.medichain.mvp.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
-        })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
+
+ function GrantAccess(grantAccess) {
+
+    var me = getCurrentParticipant();
+
+    console.log('**** AUTH: ' + me.getIdentifier + 'granting access to ' + grantAccess.accessorId)
+
+    if(!me) {
+        throw new Error('A participant/certificate mapping does not exist.');
+    }
+
+    // if the member is not already authorized, we authorize them
+    var index = -1;
+
+    if(!me.authorized) {
+        me.authorized = [];
+    }
+    else {
+        index = me.authorized.indexOf(grantAccess.accessorId);
+    }
+
+    if(index < 0) {
+        me.authorized.push(grantAccess.accessorId);
+
+        return getParticipantRegistry('org.medichain.mvp.DataAccessor')
+        .then(function (memberRegistry) {
+
+            // emit an event
+            var event = getFactory().newEvent('org.medichain.mvp', 'MemberEvent');
+            event.permissionControl = grantAccess;
+            emit(event);
+
+            // persist the state of the member
+            return memberRegistry.update(me);
         });
+    }
 }
+ 
