2b224df5e5f52aff9efddbfd9fc7af4a20b46826
nicedeve <bdawn0878@gmail.com>
2018-03-20 13:13:02 +0000

    added function for revoking access

--- medichain/lib/logic.js
+++ medichain/lib/logic.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * A medical practicioner grants access to Data Accessors / Other participants
+ * A medical practicioner GRANTS access to Data Accessors / Other participants
  * @param {org.medichain.mvp.GrantAccess} grantAccess - the function to grant access
  * @transaction
  */
@@ -42,3 +42,37 @@
     }
 }
  
+
+/**
+ * A medical practicioner REVOKES access to Data Accessors / Other participants
+ * @param {org.medichain.mvp.RevokeAccess} revoke - the RevokeAccess to be processed
+ * @transaction
+ */
+function revokeAccess(revoke) {
+
+    var me = getCurrentParticipant();
+    console.log('**** REVOKE: ' + me.getIdentifier() + ' revoking access to ' + revoke.accessorId );
+
+    if(!me) {
+        throw new Error('A participant/certificate mapping does not exist.');
+    }
+
+    // if the member is authorized, we remove them
+    var index = me.authorized ? me.authorized.indexOf(revoke.accessorId) : -1;
+
+    if(index>-1) {
+        me.authorized.splice(index, 1);
+
+        return getParticipantRegistry('org.medichain.mvp.DataAccessor')
+        .then(function (memberRegistry) {
+
+            // emit an event
+            var event = getFactory().newEvent('org.medichain.mvp', 'MemberEvent');
+            event.permissionControl = revoke;
+            emit(event);
+
+            // persist the state of the member
+            return memberRegistry.update(me);
+        });
+    }
+}
